04d9ad5304cb6d8303c302889ebc595914f927b7
locnt19 <locnt19@gmail.com>
2021-03-20 22:21:02 +0700

    change password, getme user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,5 @@
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const validator = require("validator");
 
 const User = require("../models/user");
@@ -150,44 +149,117 @@ exports.login = async (req, res) => {
 };
 
 exports.changePassword = (req, res) => {
-  res.json(req.body);
-};
+  const { _userAccess, password, newPassword } = req.body;
 
-exports.getMe = (req, res) => {
-  const token = req.headers.authorization?.split(" ");
-  if (token) {
-    if (token[0] === process.env.TOKEN_TYPE) {
-      try {
-        const verified = jwt.verify(token[1], process.env.SECRET_KEY);
-        User.findById(verified._id)
-          .select("-password -__v")
-          .then(user =>
-            res
-              .status(constant.STATUS.CODE_200)
-              .json({ responseCode: constant.STATUS.CODE_200, data: [user] })
-          )
-          .catch(error =>
-            res.status(constant.STATUS.CODE_500).json({
-              responseCode: constant.STATUS.CODE_500,
-              error: [constant.ERROR.SOMETHING]
+  if (
+    password &&
+    newPassword &&
+    !validator.isEmpty(password) &&
+    !validator.isEmpty(newPassword) &&
+    password.length >= process.env.PASSWORD_MIN_LENGTH &&
+    newPassword.length >= process.env.PASSWORD_MIN_LENGTH
+  ) {
+    if (password !== newPassword) {
+      User.findOne({ _id: _userAccess._id, isDeleted: false })
+        .then(user => {
+          bcrypt
+            .compare(password, user.password)
+            .then(matched => {
+              if (matched) {
+                user.password = newPassword;
+                user
+                  .save()
+                  .then(result =>
+                    res.status(constant.STATUS.CODE_200).json({
+                      responseCode: constant.STATUS.CODE_200,
+                      message: constant.RESPONSE.MESSAGE_UPDATED.replace(
+                        "{document}",
+                        "password"
+                      )
+                    })
+                  )
+                  .catch(error =>
+                    res.status(constant.STATUS.CODE_500).json({
+                      responseCode: constant.STATUS.CODE_500,
+                      error: [constant.ERROR.SOMETHING]
+                    })
+                  );
+              } else {
+                res.status(constant.STATUS.CODE_400).json({
+                  responseCode: constant.STATUS.CODE_400,
+                  error: [constant.ERROR.FIELD.INCORRECT_PASSWORD]
+                });
+              }
             })
-          );
-      } catch (error) {
-        res.status(constant.STATUS.CODE_400).json({
-          responseCode: constant.STATUS.CODE_400,
-          error: [constant.ERROR.AUTHOR.TOKEN_EXPIRED]
+            .catch(error => {
+              res.status(constant.STATUS.CODE_500).json({
+                responseCode: constant.STATUS.CODE_500,
+                error: [constant.ERROR.SOMETHING]
+              });
+            });
+        })
+        .catch(error => {
+          res.status(constant.STATUS.CODE_400).json({
+            responseCode: constant.STATUS.CODE_400,
+            error: [constant.ERROR.AUTHOR.NO_EXIST_USER]
+          });
         });
-      }
     } else {
       res.status(constant.STATUS.CODE_400).json({
         responseCode: constant.STATUS.CODE_400,
-        error: [constant.ERROR.AUTHOR.INCORRECT_AUTHORIZATION_TYPE]
+        error: [constant.ERROR.FIELD.PASSWORD_THE_SAME]
       });
     }
   } else {
-    res.status(constant.STATUS.CODE_401).json({
-      responseCode: constant.STATUS.CODE_401,
-      error: [constant.ERROR.AUTHOR.NO_AUTHORIZATION]
+    const errorList = [];
+
+    if (!password || validator.isEmpty(password)) {
+      errorList.push(
+        constant.ERROR.FIELD.FIELD_REQUIRED.replace("{field}", "password")
+      );
+    } else if (password.length < process.env.PASSWORD_MIN_LENGTH) {
+      errorList.push(
+        constant.ERROR.FIELD.FIELD_MIN_LENGTH.replace(
+          "{field}",
+          "password"
+        ).replace("{length}", process.env.PASSWORD_MIN_LENGTH)
+      );
+    }
+
+    if (!newPassword || validator.isEmpty(newPassword)) {
+      errorList.push(
+        constant.ERROR.FIELD.FIELD_REQUIRED.replace("{field}", "newPassword")
+      );
+    } else if (newPassword.length < process.env.PASSWORD_MIN_LENGTH) {
+      errorList.push(
+        constant.ERROR.FIELD.FIELD_MIN_LENGTH.replace(
+          "{field}",
+          "newPassword"
+        ).replace("{length}", process.env.PASSWORD_MIN_LENGTH)
+      );
+    }
+
+    res.status(constant.STATUS.CODE_400).json({
+      responseCode: constant.STATUS.CODE_400,
+      error: errorList
     });
   }
+};
+
+exports.getMe = (req, res) => {
+  const { _userAccess } = req.body;
+
+  User.findById(_userAccess._id)
+    .select("-password -__v")
+    .then(user =>
+      res
+        .status(constant.STATUS.CODE_200)
+        .json({ responseCode: constant.STATUS.CODE_200, data: [user] })
+    )
+    .catch(error =>
+      res.status(constant.STATUS.CODE_500).json({
+        responseCode: constant.STATUS.CODE_500,
+        error: [constant.ERROR.SOMETHING]
+      })
+    );
 };
