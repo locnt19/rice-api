a6b46055614a2a188362993645c60aee1bde96ee
locnt19 <locnt19@gmail.com>
2021-03-20 22:22:06 +0700

    authorization user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,32 @@
 const router = require("express").Router();
 
+const { authorization, isAdmin } = require("../middleware/auth");
+
 const { userController } = require("../controllers");
 
-const userRoutes = require("./user");
 const productCategoryRoutes = require("./product-category");
 const articleCategoryRoutes = require("./article-category");
 const productRoutes = require("./product");
 
-router.use("/user", userRoutes);
 router.use("/product-category", productCategoryRoutes);
 router.use("/article-category", articleCategoryRoutes);
 router.use("/product", productRoutes);
 
-router.get("/me", userController.getMe);
+router.get("/user", userController.findWithQuery);
+router.get("/me", authorization, userController.getMe);
 
 router.post("/login", userController.login);
 router.post("/register", userController.createUser);
-router.post("/delete/user/:id", userController.deleteUser);
+router.post(
+  "/delete/user/:id",
+  authorization,
+  isAdmin,
+  userController.deleteUser
+);
+router.post(
+  "/me/change-password",
+  authorization,
+  userController.changePassword
+);
 
 module.exports = router;
--- src/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { userController } = require("../controllers");
-
-router.get("", userController.findWithQuery);
-router.get("/change-password", userController.changePassword);
-
-module.exports = router;
